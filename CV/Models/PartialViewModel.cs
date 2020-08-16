d106b37ea7864b93f38cbde03b86b8a87694e5b2
Michael Silver <masilver@gmail.com>
2020-08-16 01:48:26 -0400

    Finished Partials

---
+++ CV/Models/PartialViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CV.Models
+{
+    public class PartialViewModel<T>
+    {
+        public bool IsForSearch { get; set; }
+        public IList<T> ItemList { get; set; }
+
+    }
+}
