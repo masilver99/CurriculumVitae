e874f7f93dfae0a494694b878f54d6295b33d46a
Michael Silver <masilver@gmail.com>
2020-09-07 01:18:38 -0400

    Cleanup

---
+++ CV/scripts/BuildApp.ps1
@@ -0,0 +1,8 @@
+﻿del published -Recurse
+dotnet publish -c Release -o published -r linux-x64 --self-contained false -p:PublishReadyToRun=true
+C:\"Program Files"\7-Zip\7z.exe a -r -mx8 published/cv.zip .\published\*
+
+Transfer to home/masilver/docker/temp/cv
+unzip into home/masilver/docker/temp/cv
+Docker build new container
+Docker compose
