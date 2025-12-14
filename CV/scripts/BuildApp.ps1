801cb6eed94ac7d278560d6e22324dbbc5168e8e
Michael Silver <masilver@gmail.com>
2025-12-14 10:02:07 -0500

    Update deployment scripts and Docker configurations; add docker-compose.yml and enhance BuildApp.ps1 for improved deployment process

--- CV/scripts/BuildApp.ps1
+++ CV/scripts/BuildApp.ps1
@@ -1,5 +1,11 @@
-﻿del published -Recurse
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]$Password
+)
+
+del published -Recurse
 dotnet publish -c Release -o published -r linux-x64 --self-contained false 
 C:\"Program Files"\7-Zip\7z.exe a -r -mx8 published/cv.zip .\published\*
-cmd.exe /c echo y | pscp -pw Everex12!! published/cv.zip masilver@michaelsilver.us:/home/masilver/docker/temp/cv.zip
-plink -ssh -t -pw Everex12!! masilver@michaelsilver.us -m scripts/hostscript.txt
+cmd.exe /c echo y | pscp -pw $Password published/cv.zip ubuntu@michaelsilver.us:/home/ubuntu/docker/sites/cv.michaelsilver.us/temp/cv.zip
+cmd.exe /c echo y | pscp -pw $Password Dockerfile-release ubuntu@michaelsilver.us:/home/ubuntu/docker/sites/cv.michaelsilver.us/temp/Dockerfile-release 
+plink -ssh -t -pw $Password ubuntu@michaelsilver.us -m scripts/hostscript.txt
